62095f341436870e48e66e1621dd8d607129efaa
Fire Chan <cken497@gmail.com>
2019-11-14 16:00:50 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.render("login.ejs", {
+    });
+    res.end();
+});
+
+app.listen(process.env.PORT || 8099);
