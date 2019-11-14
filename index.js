7ec36b088f9e5e18ddf9a1c7abc993d7d82cfb9a
Fire Chan <cken497@gmail.com>
2019-11-14 18:54:16 +0800

    edit login page

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+
+app.use(express.static(__dirname + '/public'));
+app.use('/bootstrap_css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/bootstrap_js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
 app.get("/", (req, res) => {
+    console.log(__dirname);
     res.render("login.ejs", {
     });
     res.end();
