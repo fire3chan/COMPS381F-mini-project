88a9e84a545aa3f7f2a04949ffde52bea0e91129
fire chan <cken497@gmail.com>
2019-11-18 11:03:40 +0800

    Add login page and db verify function

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const verifyPage = require('./routes/verify');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// static file setting
 app.use(express.static(__dirname + '/public'));
 app.use('/bootstrap_css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/bootstrap_js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
+// path handling
 app.get("/", (req, res) => {
-    console.log(__dirname);
     res.render("login.ejs", {
     });
     res.end();
 });
+
+app.post("/verify",   verifyPage);
 
 app.listen(process.env.PORT || 8099);
