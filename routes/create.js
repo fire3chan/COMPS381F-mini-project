17ea3fa11c7b16fcfdb4c40226a0b3fe8a296ecd
Fire Chan <cken497@gmail.com>
2019-11-27 09:48:32 +0800

    add create restaurant function

--- routes/create.js
+++ routes/create.js
@@ -1,107 +1,109 @@
 const assert = require("assert");
 const MongoClient = require("mongodb").MongoClient;
-const express = require('express');
-const app = express();
 const fs = require('fs'); //require file system
 const formidable = require('formidable');
 
-//app.use(fileUpload());
 
 const run = (req, res) => {
 
 	const dbLink = 'mongodb://student:std9870@cluster0-shard-00-00-pdydm.mongodb.net:27017,cluster0-shard-00-01-pdydm.mongodb.net:27017,cluster0-shard-00-02-pdydm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
-    //const dbLink = "mongodb+srv://firework:dark0411@cluster0-sbkrx.azure.mongodb.net/test?retryWrites=true&w=majority";
-    const dbName = "test";
-    const client = new MongoClient(dbLink);
-   
-   let form = new formidable.IncomingForm();
-   
-	form.parse(req, (err, fields, files) =>{
-		console.log(JSON.stringify(files));
-	
-		/*if (files.photo.size == 0) {
-		  //res.status(500).end("No file uploaded!");  
-		}*/
-		
-		//get filepath
-		let filename = files.photo.path;
-		
-		//get file upload type
-		if (files.photo.type) {
-		  var mimetype = files.photo.type;
-		  console.log(`mimetype = ${mimetype}`);
-		}
-		
-		//check upload file is image
-		if (!mimetype.match(/^image/)) {
-		  res.status(500).end("Upload file not image!");
-		  return;
-		}	
-	
-		//file system read file
-		fs.readFile(filename, (err,data) => {
-		  let client = new MongoClient(mongourl);
-			client.connect((err) => {
-			try {
-			  assert.equal(err,null);
-			} catch (err) {
-			  //res.status(500).end("MongoClient connect() failed!");
-			}
-			const db = client.db(dbName);
-			let restaurant = {};
-			restaurant['name'] = fields.title;
-			restaurant['borough'] = fields.borough;
-			restaurant['cuisine'] = fields.cuisine;
-			restaurant['street'] = fields.street;
-			restaurant['building'] = fields.building;
-			restaurant['zipcode'] = fields.zipcode;
-			restaurant['lon'] = fields.lon;
-			restaurant['lat'] = fields.lat;
-			restaurant['mimetype'] = mimetype;
-			restaurant['image'] = new Buffer.from(data).toString('base64');
-			
-			insertRestaurant(db, restaurant,(result) => {
-			  
-				client.close();
-			  
-			  	res.render('displayItem.ejs',{
-				restName: restaurant['name'],
-				mimetype: restaurant['mimetype'],			
-				photo:  restaurant['image'],  
-							  
-				boro: restaurant['borough'],
-				cuis: restaurant['cuisine'],
-				street: restaurant['street'],
-				build: restaurant['lon'],
-				zipcode: restaurant['zipcode'],
-				gps: [restaurant['lon'], restaurant['lat']],
-				rate: "",
-				owner: "",
-							  
-				//rateWithId: "/rate?_id="+docObj._id,
-				//editWithId: "/change?_id="+docObj._id,
-				 //delWithId: "/remove?_id="+docObj._id
-				 
-				rateWithId: "",
-				editWithId: "",
-				delWithId: ""
-				})
-				
-				//res.status(200).end('Restaurant was inserted into MongoDB!');  
-			});
-			
-		  });
+	//const dbLink = "mongodb+srv://firework:dark0411@cluster0-sbkrx.azure.mongodb.net/test?retryWrites=true&w=majority";
+	const dbName = "test";
+	const client = new MongoClient(dbLink);
+
+	let form = new formidable.IncomingForm();
+
+	let restaurant = {};
+
+	const insertRestaurant = (db, restaurant, callback) => {
+		db.collection('prorestaurant').insertOne(restaurant, (err, result) => {
+			assert.equal(err, null);
+			console.log("insert was successful!");
+			console.log(JSON.stringify(result));
+			callback();
 		});
-	});   
-	
-	function insertRestaurant(db,r,callback) {
-	  db.collection('prorestaurant').insertOne(r,function(err,result) {
-		assert.equal(err,null);
-		console.log("insert was successful!");
-		console.log(JSON.stringify(result));
-		callback(result);
-	  });
 	}
+
+	const getRestaurantId = (db, callback) => {
+		db.collection('prorestaurant').count((error, result) => {
+			assert.equal(error, null);
+			callback(result);
+		})
+	}
+
+	form.parse(req, (err, fields, files) => {
+		assert.equal(err, null);
+
+		console.log('Fields', fields);
+		let photo = files.photo;
+		console.log(photo);
+		let filename = photo.path;
+
+		if (photo.size === 0) {
+			console.log("No file");
+			res.setHeader("500", { "Content-Type": "plain/html" });
+			res.send("No file uploaded!");
+			res.end();
+			return;
+		}
+
+		if (photo.type) {
+			//check upload file is image
+			if (!photo.type.match(/^image/)) {
+				res.setHeader("500", { "Content-Type": "plain/html" });
+				res.send("Upload file not image!");
+				res.end();
+				return;
+			}
+			restaurant["photo_mimetype"] = photo.type;
+		}
+
+		fs.readFile(filename, (err, data) => {
+			assert.equal(err, null);
+			restaurant['photo'] = new Buffer.from(data).toString('base64');
+
+			client.connect((err) => {
+				try {
+					assert.equal(err, null);
+				} catch (err) {
+					res.writeHead(500, { "Content-Type": "plain/html" });
+					res.send("MongoClient connect() failed!");
+					res.end();
+					return;
+				}
+
+				const db = client.db(dbName);
+				getRestaurantId(db, (result) => {
+					restaurant['restaurant_id'] = result;
+					restaurant['name'] = fields.name;
+					restaurant['borough'] = fields.borough;
+					restaurant['cuisine'] = fields.cuisine;
+					restaurant.address = {};
+					restaurant.address['street'] = fields.street;
+					restaurant.address['building'] = fields.building;
+					restaurant.address['zipcode'] = fields.zipcode;
+					restaurant.address['coord'] = fields.latitude + ", " + fields.longitude;
+					restaurant['owner'] = req.cookies.session;
+
+					insertRestaurant(db, restaurant, () => {
+						// res.setHeader("Content-Type", "plain/html");
+						// console.log("write head");
+						// res.writeHead(200);
+						// console.log("send");
+						// res.send("Restaurant was inserted into MongoDB!");
+						// console.log("close");
+						client.close();
+						// console.log("end");
+						res.status(200).end('Restaurant was inserted into MongoDB!');
+						// res.end();
+
+					});
+				});
+			})
+		});
+
+	});
+
 }
 
 
