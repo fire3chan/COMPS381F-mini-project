fb08a4eaff1a03705f4fc3ab82d250335dd18582
Fire Chan <cken497@gmail.com>
2019-12-02 00:42:54 +0800

    fix style

--- routes/gotoUpdate.js
+++ routes/gotoUpdate.js
@@ -1,66 +1,47 @@
-// const run = (req, res) => {
-//     if (req.cookies.session !== null && req.cookies.session !== "" && req.cookies.session !== undefined) {
-//         res.render("update.ejs", { sessionName: req.cookies.session, _id: req.query._id});
-//         console.log(req.query.id);
-//     } else {
-//         res.redirect("/");
-//     }
-
-// };
-
-// module.exports = run;
-
-
 const assert = require("assert");
 const MongoClient = require("mongodb").MongoClient;
-const url = require('url');
-const ObjectID = require('mongodb').ObjectID;
+const ObjectID = require("mongodb").ObjectID;
 
 const run = (req, res) => {
 
-    const dbLink = 'mongodb://student:std9870@cluster0-shard-00-00-pdydm.mongodb.net:27017,cluster0-shard-00-01-pdydm.mongodb.net:27017,cluster0-shard-00-02-pdydm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+    const dbLink = "mongodb://student:std9870@cluster0-shard-00-00-pdydm.mongodb.net:27017,cluster0-shard-00-01-pdydm.mongodb.net:27017,cluster0-shard-00-02-pdydm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
     const dbName = "test";
     const client = new MongoClient(dbLink);
 
-    let parsedURL = url.parse(req.url, true);
-    let abc = new ObjectID(req.query._id);
+    let restaurantIdObj = new ObjectID(req.query._id);
 
-    const findRestaurants = (db, callback) => {
-       db.collection('prorestaurant').findOne({ "_id" : abc }, (err,result)=> {
-      
-        const restaurantObj = result
-        callback(restaurantObj);
-        
-    });
-    }
-		client.connect((err) => {
-			assert.equal(null, err);
-			console.log("successful to connect to db!");
-
-			const db = client.db(dbName);
-
-			findRestaurants(db, (restaurants) => {
-                var co = restaurants.address.coord;
-					let str = [];
-					str=co.split(",");
-					var lat = parseInt(str[0]);
-                    var lot = parseInt(str[1]);
-				res.render('update.ejs', {
-					sessionName: req.cookies.session,
-                    _id: req.query._id,
-                    restaurants: restaurants,
-                    lat : lat,
-                    lot : lot
-                    
-
-                });
-                res.end();
-                client.close();
-			});
-
+    const findRestaurant = (db, callback) => {
+        db.collection("prorestaurant").findOne({ "_id": restaurantIdObj }, (err, result) => {
+            assert.equal(null, err);
+            const restaurantObj = result;
+            callback(restaurantObj);
         });
-    }
+    };
 
+    client.connect((err) => {
+        assert.equal(null, err);
+        console.log("successful to connect to db!");
 
+        const db = client.db(dbName);
+
+        findRestaurant(db, (restaurant) => {
+            let coord = restaurant.address.coord;
+            let str = [];
+            str = coord.split(",");
+            let lat = parseInt(str[0]);
+            let lot = parseInt(str[1]);
+            res.render("update.ejs", {
+                sessionName: req.cookies.session,
+                _id: req.query._id,
+                restaurant: restaurant,
+                lat: lat,
+                lot: lot
+            });
+            res.end();
+            client.close();
+        });
+
+    });
+}
 
 module.exports = run;
